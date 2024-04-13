ffc02c1aa6cce31f6189dfb4e6c1ce8bbf114097
dm5561886 <wan0921139081@gmail.com>
2024-04-13 16:45:09 +0800

    add 第二版

--- 口罩資料 API 串接.py
+++ 口罩資料 API 串接.py
@@ -5,9 +5,19 @@ url = 'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.jso
 response = requests.get(url)
 
 # 將請求回應的內容存成一個字串格式
-d = response.text
+# response.text
 
 # 將長得像 json 格式的字串解析成字典或列表
-data = json.loads(d)
+data = json.loads(response.text)
 
-print(data)
+med_count = {}
+# 填入欄位名稱
+for d in data['features']:
+    conunty = d['properties']['address'][:3]
+    if conunty not in med_count:
+        med_count[conunty] = 0
+    else:
+        med_count[conunty] += 1
+
+print(med_count)
+# {'台北市': 123, '新北市': 456 ...}
