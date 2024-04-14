4b4cf1cbe5985c3e424152cc6c836ed7df6b8d92
dm5561886 <wan0921139081@gmail.com>
2024-04-14 23:38:08 +0800

    add 第二版

--- 將資料存入資料庫/將資料存入資料庫.py
+++ 將資料存入資料庫/將資料存入資料庫.py
@@ -5,9 +5,15 @@ import datetime
 
 conn = sqlite3.connect('example.db')
 c = conn.cursor()
+
+# 新增且清空資料表
 c.execute('''CREATE TABLE IF NOT EXISTS pharmacies
              (city text, counts text, createdAt datetime)''')
 c.execute('''DELETE FROM pharmacies''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS masks
+             (maskcity text, maskcount text, createdAt datetime)''')
+c.execute('''DELETE FROM masks''')
 conn.commit()
 
 # 利用 requests 對 API 來源發送一個請求
@@ -36,9 +42,30 @@ for i in med_count.items():
     c.execute(f"INSERT INTO pharmacies VALUES ('{city}', {counts}, '{t}')")
     conn.commit()
 
+# 計算出每個地區的成人剩餘口罩數量
+mask_count = {}
+for d in data['features']:
+    conunty = d['properties']['address'][:3]
+    if conunty not in mask_count:
+        mask_count[conunty] = 0
+    else:
+        mask_adult = d['properties']['mask_adult']
+        mask_count[conunty] += mask_adult
+
+for j in mask_count.items():
+    maskcity = j[0]
+    maskcount = j[1]
+    t2 = datetime.datetime.now()
+    c.execute(
+        f"INSERT INTO masks VALUES ('{maskcity}', {maskcount}, '{t2}')")
+    conn.commit()
+
 # 查詢資料
 c.execute("SELECT * FROM pharmacies")
-print(c.fetchall())  # 取得所有查詢結果的資料
+print(c.fetchall())
+
+c.execute("SELECT * FROM masks")
+print(c.fetchall())
 
 conn.commit()
 conn.close()
