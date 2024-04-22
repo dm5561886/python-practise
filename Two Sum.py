5ffea87bca0ca41e15753ace816b8ed7274fb48a
dm5561886 <wan0921139081@gmail.com>
2024-04-22 18:35:25 +0800

    方法二

--- Two Sum.py
+++ Two Sum.py
@@ -1,12 +1,14 @@
-# 方法一(無函式)
-nums = [2, 7, 11, 15]
-target = 18
-n = len(nums)
-d = []
-for i in range(n):
-    for j in range(i+1, n):
-        if nums[i]+nums[j] == target:
-            d.append(i)
-            d.append(j)
+# 方法二
+def twosum(nums, target):
+    for i in range(n):
+        for j in range(i+1, n):
+            if nums[i]+nums[j] == target:
+                return [i, j]
+    return []
 
-print(d)  # [0,1]
+
+nums = [2, 7, 11, 15]
+target = 9
+n = len(nums)
+d = twosum(nums, target)
+print(d)
