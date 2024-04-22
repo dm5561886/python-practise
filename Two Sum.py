d622652864fc7ffb0a6fdd1cb11893997d2e6d4f
dm5561886 <wan0921139081@gmail.com>
2024-04-22 19:21:58 +0800

    方法三

--- Two Sum.py
+++ Two Sum.py
@@ -1,14 +1,17 @@
-# 方法二
+# 方法三
 def twosum(nums, target):
-    for i in range(n):
-        for j in range(i+1, n):
-            if nums[i]+nums[j] == target:
-                return [i, j]
+    x = {}
+    for i, num in enumerate(nums):  # 使用enumerate()函數轉換為索引序列
+        diff = target-num  # 計算與目標值的差值
+        if diff in x:
+            return [x[diff], i]
+        x[num] = i  # 將當前數字和 index 存入 visited 中
+
     return []
 
 
 nums = [2, 7, 11, 15]
-target = 9
+target = 26
 n = len(nums)
 d = twosum(nums, target)
 print(d)
