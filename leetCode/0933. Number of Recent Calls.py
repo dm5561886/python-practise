86d85d13a61dbccd69769483d7b1932dae21bec5
dm5561886 <wan0921139081@gmail.com>
2024-07-28 14:28:30 +0800

    upload file

---
+++ leetCode/0933. Number of Recent Calls.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+class RecentCounter:
+    def __init__(self):
+        # 建立佇列存放空間
+        self.queue = deque()
+
+    def ping(self, t: int) -> int:
+        queue = self.queue
+        # 把每次呼叫 ping 時間點紀錄到 deque
+        queue.append(t)
+        # 當第一個紀錄的時間小於3000毫秒，
+        while queue[0] < t - 3000:
+            queue.popleft()
+        return len(queue)
+
+
+# 測試程式碼
+if __name__ == "__main__":
+    recentCounter = RecentCounter()
+    print(recentCounter.ping(1))    # 輸出應為 1
+    print(recentCounter.ping(100))  # 輸出應為 2
+    print(recentCounter.ping(3001))  # 輸出應為 3
+    print(recentCounter.ping(3002))  # 輸出應為 3
